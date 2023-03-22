8099e4dafab82a98895a26827bc891dda1d2804b
Amir <73949126+amirfarajii@users.noreply.github.com>
2023-03-22 15:14:25 +0330

    Delete contracts/mainnet/0c directory

--- contracts/mainnet/0c/0Cc87521533771cc68FE079895a3AA38A2e56B0E_Sketch.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
-import "ERC20.sol";
-
-contract Sketch is ERC20 {
-    constructor () public ERC20 ("Sketch Finance", "Sketch"){
-        _mint(msg.sender, 10000000 * (10 ** uint256(decimals())));
-    }
-}
